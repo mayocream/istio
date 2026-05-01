470fbd1bdb90c91615a25eef89aebbf9b3c4b2a9
Krinkin, Mike <mkrinkin@microsoft.com>
2026-05-01 18:52:12 +0200

    Make gateway and listenerset status reporting comply with Gateway API (#60063)
    * Populate confromance test timeouts with defaults
    
    Until now we basically relied on the assumption that when timeout is set
    to 0, Gateway API conformance tests will populate it with some sensible
    default.
    
    NOTE: In gateway API conformance test suite this happens inside
    [SetupTimeoutConfig](https://github.com/kubernetes-sigs/gateway-api/blob/efb744260101b06ffa3cce7cbe41d48bc614de34/conformance/utils/config/timeout.go#L221C6-L280), which in turn is called inside
    [NewConformanceTestSuite](https://github.com/kubernetes-sigs/gateway-api/blob/efb744260101b06ffa3cce7cbe41d48bc614de34/conformance/utils/suite/suite.go#L262).
    
    That's a reasonable assumption, but for some of the timeouts it does not
    hold, specifically for `ListenerSetMustHaveCondition` it's not true and
    that's why `ListenerSetAllowedNamespaceNone` and
    `ListenerSetAllowedNamespaceSame` seem to be failing, even though the
    functionality is there.
    
    I belive it's a bug in the gateway API conformance test harness, but
    it's faster to fix it in Istio repo, plus looking at the code, I can see
    how this type of bug could happen, so I wouldn't be surprised if it
    happens again at some point in the future.
    
    With that in mind, this PR populates the timeouts with the default
    values explicitly instead of relying on Gateway API to replace zero
    values with defaults, thus making things slightly more robust.
    
    Signed-off-by: Mikhail Krinkin <mkrinkin@microsoft.com>
    
    * Fix lint warnings
    
    Signed-off-by: Mikhail Krinkin <mkrinkin@microsoft.com>
    
    * Skip fixed ListenerSet tests in agentgateway - they fail there for a different reason
    
    Signed-off-by: Mikhail Krinkin <mkrinkin@microsoft.com>
    
    * Stop skipping ListenerSetAllowedNamespaceSelector
    
    Signed-off-by: Mikhail Krinkin <mkrinkin@microsoft.com>
    
    * Skip ListenerSetAllowedNamespaceSelector for agw
    
    Signed-off-by: Mikhail Krinkin <mkrinkin@microsoft.com>
    
    * Fix formatting
    
    Signed-off-by: Mikhail Krinkin <mkrinkin@microsoft.com>
    
    * Make gateway and listenerset status reporting comply with Gateway API
    
    There are a couple of issues in the status reporting currently that this
    PR fixes:
    - Istio reports the number of listeners instead of listenersets for
      gateway in AttachedListenerSets field
    - Istio does not report the number of attached routes for listeners in
      listenersets.
    
    This PR changes the status reporting to fix those issues and with that
    it makes the following conformance tests pass:
    - ListenerSetAllowedRoutesNamespaces
    - ListenerSetAllowedRoutesSupportedKinds
    - ListenerSetDefaultNotAllowed
    - ListenerSetHTTPRouting
    
    There are still a few ListenerSet conformance tests in gateway API 1.5.0
    that are failing for Istio, but those fail for different reasons and
    will be addressed separately.
    
    Related to #60018
    
    Signed-off-by: Mikhail Krinkin <mkrinkin@microsoft.com>
    
    * Add a release note, since it changes user visible behavior a bit
    
    Signed-off-by: Mikhail Krinkin <mkrinkin@microsoft.com>
    
    * Update test goldens
    
    Signed-off-by: Mikhail Krinkin <mkrinkin@microsoft.com>
    
    ---------
    
    Signed-off-by: Mikhail Krinkin <mkrinkin@microsoft.com>

--- tests/integration/pilot/gateway_conformance_test.go
+++ tests/integration/pilot/gateway_conformance_test.go
@@ -87,13 +87,9 @@ var skippedTests = map[string]string{
 	"HTTPRouteCORS":                                   "TODO",
 	"HTTPRouteHTTPSListenerDetectMisdirectedRequests": "TODO",
 
-	"ListenerSetAllowedRoutesNamespaces":     "TODO",
-	"ListenerSetAllowedRoutesSupportedKinds": "TODO",
-	"ListenerSetDefaultNotAllowed":           "TODO",
-	"ListenerSetHostnameConflict":            "TODO",
-	"ListenerSetHTTPRouting":                 "TODO",
-	"ListenerSetProtocolConflict":            "TODO",
-	"ListenerSetReferenceGrant":              "TODO",
+	"ListenerSetHostnameConflict": "TODO",
+	"ListenerSetProtocolConflict": "TODO",
+	"ListenerSetReferenceGrant":   "TODO",
 
 	"MeshHTTPRoute303Redirect": "TODO",
 	"MeshHTTPRoute307Redirect": "TODO",
@@ -112,9 +108,13 @@ var agentgatewaySkippedTests = map[string]string{
 	"TLSRouteTerminateSimpleSameNamespace":  "TODO",
 	"TLSRouteMixedTerminationSameNamespace": "TODO",
 
-	"ListenerSetAllowedNamespaceNone":     "TODO",
-	"ListenerSetAllowedNamespaceSame":     "TODO",
-	"ListenerSetAllowedNamespaceSelector": "TODO",
+	"ListenerSetAllowedNamespaceNone":        "TODO",
+	"ListenerSetAllowedNamespaceSame":        "TODO",
+	"ListenerSetAllowedNamespaceSelector":    "TODO",
+	"ListenerSetAllowedRoutesNamespaces":     "TODO",
+	"ListenerSetAllowedRoutesSupportedKinds": "TODO",
+	"ListenerSetDefaultNotAllowed":           "TODO",
+	"ListenerSetHTTPRouting":                 "TODO",
 }
 
 func TestGatewayConformance(t *testing.T) {
